d9b8168a95fd18f6b7e302578c2e19fcd292fe25
kieran <k>
2019-10-24 19:24:53 +0100

    Added square class and constructer and commented every class so far

--- Dungeon Crawler/Assets/Test Scripts/DungeonMeshGen.cs
+++ Dungeon Crawler/Assets/Test Scripts/DungeonMeshGen.cs
@@ -2,8 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DungeonMeshGen : MonoBehaviour
 {
+    /// <summary>
+    /// Class and constructor for Squares. These squares are equivelent to the map tiles but are shrunken anchored on the map tile's centre coordinate.
+    /// </summary>
+    public class Square
+    {
+        /// <summary>
+        /// The control nodes monitor whether the tile in each of the neighbouring diagonal tiles are a wall.
+        /// </summary>
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        /// <summary>
+        /// The nodes of each orthogonal direction from the centre of the square.
+        /// </summary>
+        public Node centreTop, centreRight, centreBottom, centreLeft;
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+        {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomRight = _bottomRight;
+            bottomLeft = _bottomLeft;
+
+            centreTop = topLeft.right;
+            centreRight = bottomRight.above;
+            centreBottom = bottomLeft.right;
+            centreLeft = bottomLeft.above;
+        }
+    }
+    /// <summary>
+    /// Class and constructor for nodes surrounding map tiles.
+    /// </summary>
     public class Node
     {
         public Vector3 position;
@@ -14,10 +45,18 @@ public class DungeonMeshGen : MonoBehaviour
             position = _pos;
         }
     }
+    /// <summary>
+    /// Class and constructor for Control nodes surrounding map tiles. These nodes are active if they diagonally border a tile which is a wall.
+    /// </summary>
     public class ControlNode : Node
     {
-
+        /// <summary>
+        /// Whether or not the Control Node borders a wall.
+        /// </summary>
         public bool active;
+        /// <summary>
+        /// The two nodes the controller node controls.
+        /// </summary>
         public Node above, right;
 
         public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
