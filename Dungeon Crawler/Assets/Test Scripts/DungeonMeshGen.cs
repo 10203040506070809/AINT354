6535ff4b73a4a033169c53b80a141d172e13ff09
kieran <k>
2019-10-24 19:28:42 +0100

    Added SquareGrid class and constructor

--- Dungeon Crawler/Assets/Test Scripts/DungeonMeshGen.cs
+++ Dungeon Crawler/Assets/Test Scripts/DungeonMeshGen.cs
@@ -5,6 +5,42 @@ using UnityEngine;
 
 public class DungeonMeshGen : MonoBehaviour
 {
+    public SquareGrid squareGrid;
+
+
+    public class SquareGrid
+    {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
+                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);
+                }
+            }
+
+            squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                }
+            }
+
+        }
+    }
     /// <summary>
     /// Class and constructor for Squares. These squares are equivelent to the map tiles but are shrunken anchored on the map tile's centre coordinate.
     /// </summary>
