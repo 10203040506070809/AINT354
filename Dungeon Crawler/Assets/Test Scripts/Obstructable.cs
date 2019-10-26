8d77d6e3b04cb918843ae177fc9afa11ae1b0a5f
Jack Fletcher <Jackfletch4@gmail.com>
2019-10-27 00:35:46 +0100

    Creation of Obstructable class

---
+++ Dungeon Crawler/Assets/Test Scripts/Obstructable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstructable : MonoBehaviour
+{
+    private Color m_transparantColour;
+    private Color m_originalColour;
+    private Renderer renderer;
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        m_originalColour = renderer.material.color;
+    }
+
+    public void SetTransparant()
+    {
+        renderer.material.color = m_transparantColour;
+    }
+    public void SetNormal()
+    {
+        renderer.material.color = m_originalColour;
+    }
+}
