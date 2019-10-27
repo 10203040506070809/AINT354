8fd3f3dd698592c295a80361e611f8508b1c40b7
Jack Fletcher <Jackfletch4@gmail.com>
2019-10-27 01:59:39 +0000

    Creation of TitleScreenManager

---
+++ Dungeon Crawler/Assets/Test Scripts/PlayerCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private GameObject sword;
+    [SerializeField] private GameObject gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///If input happens
+        if (Input.GetButton("Fire1"))
+        {
+            ///Do animation
+            
+        }
+    }
+}
