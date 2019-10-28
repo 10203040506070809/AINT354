cac574a41ce58bc3625937331b140a272d2dbc1c
Jack Fletcher <Jackfletch4@gmail.com>
2019-10-28 14:50:28 +0000

    Creation of m_lookRadius float

--- Dungeon Crawler/Assets/Test Scripts/EnemyController.cs
+++ Dungeon Crawler/Assets/Test Scripts/EnemyController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : CharacterMovement
 {
+    [SerializeField] private float m_lookRadius;
     /// <summary>
     /// 
     /// </summary>
