1eb45b56f26550782ee5f3a390b2b9b511824fd5
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-01 23:27:48 +0000

    Removed Start method from Interactable

--- Dungeon Crawler/Assets/Test Scripts/Interactable.cs
+++ Dungeon Crawler/Assets/Test Scripts/Interactable.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     /// <summary>
     /// A method which is activated whenever a gameobject with this is interacted with. Meant to be overriden.
