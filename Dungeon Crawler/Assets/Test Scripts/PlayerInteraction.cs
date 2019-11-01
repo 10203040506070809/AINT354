4a83f27ec98d701d83ea8af17e8b7f06005ab9e7
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-01 22:38:34 +0000

    Creation of Vector3 m_Direction

--- Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
+++ Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
 
-
+    private Vector3 m_direction; 
     /// <summary>
     /// Called at the same time once per frame. 
     /// </summary>
