76cd1d215d5bf919c5f380c51cd7ca0abda68972
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-01 22:40:35 +0000

    Added a raycast check in m_direction to m_maxDistance

--- Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
+++ Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-
+    /// <summary>
+    /// 
+    /// </summary>
     private Vector3 m_direction;
+    /// <summary>
+    /// 
+    /// </summary>
     [SerializeField] private float m_maxDistance;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        
+    }
     /// <summary>
     /// Called at the same time once per frame. 
     /// </summary>
     private void FixedUpdate()
     {
-        
+        if (Physics.Raycast(transform.position, m_direction, m_maxDistance))
+        {
+
+        }
     }
 }
