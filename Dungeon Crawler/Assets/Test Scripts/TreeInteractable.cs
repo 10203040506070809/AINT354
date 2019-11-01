cce559db3c96fc3bf91f53d25fc75d8a630beded
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-01 23:26:20 +0000

    Created a test interaction. Can be used for some small easter egg later along the line

--- Dungeon Crawler/Assets/Test Scripts/TreeInteractable.cs
+++ Dungeon Crawler/Assets/Test Scripts/TreeInteractable.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class TreeInteractable : Interactable
 {
-
+    /// <summary>
+    /// Overridden method. Do object specific interactions inside this method.
+    /// </summary>
+    public override void InteractedWith()
+    {
+        base.InteractedWith();
+        
+    }
 }
