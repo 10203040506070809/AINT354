936908d9d193271b81d97aea91fc4f83ec47f6c9
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-01 23:06:40 +0000

    Added OnDrawGizmos so a developer can see the distance the player can see in the editor.

--- Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
+++ Dungeon Crawler/Assets/Test Scripts/PlayerInteraction.cs
@@ -11,20 +11,21 @@ public class PlayerInteraction : MonoBehaviour
     /// <summary>
     /// 
     /// </summary>
-    [SerializeField] private float m_maxDistance;
+    [SerializeField] private float m_maxDistance = 0;
 
     /// <summary>
     /// 
     /// </summary>
     private void Start()
     {
-        m_direction = transform.TransformDirection(Vector3.forward);
+        
     }
     /// <summary>
     /// Called at the same time once per frame. Checks in a direction for  an interactable object.
     /// </summary>
     private void FixedUpdate()
     {
+        m_direction = transform.TransformDirection(Vector3.forward);
         RaycastHit hit;
 
         
@@ -39,5 +40,13 @@ public class PlayerInteraction : MonoBehaviour
             }
             
         }
+    }
+    /// <summary>
+    /// Draws a wireframe line from the character to easily see its 
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position,new Vector3(0,0,m_maxDistance));
     }
 }
