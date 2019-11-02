a8bac76bf47c6b00ace47375a88cff060f22c15f
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2019-11-02 23:43:37 +0000

    Added enemy spawn script

---
+++ Dungeon Crawler/Assets/Test Scripts/SpawnObjectInRoom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectInRoom : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject pickUp;
+    private int maxEnemy = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 centre = gameObject.transform.position;
+            if (Random.Range(0,100) < 100/3)
+            {
+                Vector3 tempPos = new Vector3(centre.x + Random.Range(0, 220), centre.y, centre.z + Random.Range(0, 220));
+                Instantiate(enemy, tempPos, Quaternion.identity);
+            }
+            if (Random.Range(0, 100) < 100 / 3)
+            {
+                Vector3 tempPos = new Vector3(centre.x - Random.Range(0, 220), centre.y, centre.z - Random.Range(0, 220));
+                Instantiate(enemy, tempPos, Quaternion.identity);
+            }
+            if (Random.Range(0, 100) < 100 / 3)
+            {
+                Vector3 tempPos = new Vector3(centre.x - Random.Range(0, 220), centre.y, centre.z + Random.Range(0, 220));
+                Instantiate(enemy, tempPos, Quaternion.identity);
+            }
+            if (Random.Range(0, 100) < 100 / 3)
+            {
+                Vector3 tempPos = new Vector3(centre.x + Random.Range(0, 220), centre.y, centre.z - Random.Range(0, 220));
+                Instantiate(enemy, tempPos, Quaternion.identity);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
