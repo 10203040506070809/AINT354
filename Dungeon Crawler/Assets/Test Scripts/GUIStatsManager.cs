c331cd4f6322f6b09cad5f5d6f37a8d341dc6fb7
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-02 15:13:56 +0000

    Added private Slider m_healthSlider

--- Dungeon Crawler/Assets/Test Scripts/GUIStatsManager.cs
+++ Dungeon Crawler/Assets/Test Scripts/GUIStatsManager.cs
@@ -5,23 +5,16 @@ using UnityEngine.UI;
 
 public class GUIStatsManager : MonoBehaviour
 {
-    [SerializeField] private Slider m_HealthSlider;
-    [SerializeField] private Slider m_CorruptionSlider;
-    [SerializeField] private PlayerStats m_PlayerStats;
+    [SerializeField] private Slider m_healthSlider;
     // Start is called before the first frame update
     void Start()
     {
-        m_HealthSlider.maxValue = m_PlayerStats.m_maxHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateGUI();
-    }
-
-    void UpdateGUI()
-    {
-
+        
     }
 }
