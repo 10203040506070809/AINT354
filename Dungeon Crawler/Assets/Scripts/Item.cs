5022f6697701572257f4e318788b88c73c96a855
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-13 01:50:30 +0000

    Replaced TestSceneJack with a coipy of Overworld

--- Dungeon Crawler/Assets/Scripts/Item.cs
+++ Dungeon Crawler/Assets/Scripts/Item.cs
@@ -8,14 +8,16 @@ public class Item : MonoBehaviour
     /// <summary>
     /// Occurs when an item interacts with this. Checks that the other GameObject is a player and if so, activeates the OnPickUp script.
     /// </summary>
-    /// <param name="other"></param>
+    /// <param name="other">A reference to the other GameObjects collider.</param>
     public virtual void OnTriggerEnter(Collider other)
     {
         
     }
-
+    /// <summary>
+    /// A method which 
+    /// </summary>
     public virtual void OnPickUp()
     {
-
+        Debug.Log("Picked up.");
     }
 }
