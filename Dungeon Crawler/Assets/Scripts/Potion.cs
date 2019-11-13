3e4c015cfa3bad3beaad244c970ea5d672c8cdf7
Jack Fletcher <33032000+10203040506070809@users.noreply.github.com>
2019-11-13 10:52:57 +0000

    Added IEnumerator PotionCooldown

--- Dungeon Crawler/Assets/Scripts/Potion.cs
+++ Dungeon Crawler/Assets/Scripts/Potion.cs
@@ -12,12 +12,32 @@ public class Potion : Item
     /// A float value, used to denote the required time between using two of the same potions.
     /// </summary>
     [SerializeField] private float m_potionCooldown;
+    /// <summary>
+    /// An integer value denoting how many of the item can be stored in a single slot.
+    /// </summary>
+    [SerializeField] private int m_maxNumberInSlot;
+    private bool m_isAvailable = true;
 
     /// <summary>
     /// Activates the potion effect.
     /// </summary>
     public virtual void ActivatePotion()
     {
-       
+       if (m_isAvailable)
+        {
+            m_isAvailable = false;
+            
+            ///Activate potion effect here
+        }
+    }
+
+    /// <summary>
+    /// Sets m_isAvailable to true after the potions cooldown.
+    /// </summary>
+    /// <returns></returns>
+    public virtual IEnumerator PotionCooldown()
+    {
+        yield return new WaitForSeconds(m_potionCooldown);
+        m_isAvailable = true;
     }
 }
