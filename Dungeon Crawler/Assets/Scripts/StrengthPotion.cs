470be8152142da8f862bbd4daa04134fc6ac1c65
Jack Fletcher <33032000+10203040506070809@users.noreply.github.com>
2019-11-13 11:28:57 +0000

    Added strength potion

---
+++ Dungeon Crawler/Assets/Scripts/StrengthPotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrengthPotion : Potion
+{
+    // Start is called before the first frame update
+
+
+    public override void OnPickUp()
+    {
+        base.OnPickUp();
+    }
+
+    public override void ActivateEffect()
+    {
+        Debug.Log("Strength potion activated");
+    }
+
+    public override void ActivatePotion()
+    {
+        base.ActivatePotion();
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+}
