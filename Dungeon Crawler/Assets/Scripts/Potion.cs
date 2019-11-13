d8b2e624401062c040f73e9ba54166d62078007c
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-13 02:08:10 +0000

    Added summary tags to potion

--- Dungeon Crawler/Assets/Scripts/Potion.cs
+++ Dungeon Crawler/Assets/Scripts/Potion.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Potion : Item
 {
+    /// <summary>
+    /// A float value denoting the duration of a potion.
+    /// </summary>
     [SerializeField] private float m_potionTimer;
+    /// <summary>
+    /// A float value, used to denote the required time between using two of the same potions.
+    /// </summary>
     [SerializeField] private float m_potionCooldown;
 
+    /// <summary>
+    /// Activates the potion effect.
+    /// </summary>
     public virtual void ActivatePotion()
     {
-
+       
     }
 }
