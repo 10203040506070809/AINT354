afb6ff7ea64dad2008eee0bb14c04b09c0a251e9
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-13 01:59:35 +0000

    Creation of  private floast m_potionCooldown

--- Dungeon Crawler/Assets/Scripts/Potion.cs
+++ Dungeon Crawler/Assets/Scripts/Potion.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Potion : Item
 {
     [SerializeField] private float m_potionTimer;
-
+    [SerializeField] private float m_potionCooldown;
 
     public virtual void ActivatePotion()
     {
