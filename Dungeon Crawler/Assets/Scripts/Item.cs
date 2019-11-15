df4b0dfec651062acd957883d0b29f15c6fd38f8
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-15 23:55:35 +0000

    Added private Image m_hotBarIcon and m_hoverOverText to Item class

--- Dungeon Crawler/Assets/Scripts/Item.cs
+++ Dungeon Crawler/Assets/Scripts/Item.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Item : MonoBehaviour
 {
     /// <summary>
     /// An integer value denoting how many of the item can be stored in a single slot in the hotbar.
     /// </summary>
     [SerializeField] private int m_maxNumberInSlot;
-
+    /// <summary>
+    /// A reference to an image variable, which is the image shown on the hotbar UI.
+    /// </summary>
+    [SerializeField] private Image m_hotBarIcon;
+    /// <summary>
+    /// A string that stores hover over text, so the player can understand what different items are and do.
+    /// </summary>
+    [SerializeField] private string m_hoverOverText;
     /// <summary>
     /// Occurs when an item interacts with this. Checks that the other GameObject is a player and if so, activeates the OnPickUp script.
     /// </summary>
