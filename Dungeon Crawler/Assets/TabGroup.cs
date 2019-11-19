8bb8193e030ee436470b398ba52715ee25be332b
Jack Fletcher <Jackfletch4@gmail.com>
2019-11-19 19:02:10 +0000

    Creation of TabButton and TabGroup scripts

---
+++ Dungeon Crawler/Assets/TabGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created using Game Dev Guide's 'Creating a Custom Tab System in Unity' https://www.youtube.com/watch?v=211t6r12XPQ
+/// </summary>
+public class TabGroup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
