7189db372f59e81177e78b8c2097bbee53879ff2
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2019-11-23 18:07:21 +0000

    barrels spawn

--- Dungeon Crawler/Assets/Scripts/SpawnObjectInRoom.cs
+++ Dungeon Crawler/Assets/Scripts/SpawnObjectInRoom.cs
@@ -13,18 +13,106 @@ public class SpawnObjectInRoom : MonoBehaviour
     /// </summary>
     [Range(0.0f, 100.0f)]
     [SerializeField] private float m_ChanceOfEnemySpawn = 0;
+    [Range(0.0f, 100.0f)]
+    [SerializeField] private float m_ChanceOfBarrelSpawn = 0;
+    [Range(0.0f, 100.0f)]
+    [SerializeField] private float m_ChanceOfSecondBarrelSpawn = 0;
+    [Range(0.0f, 100.0f)]
+    [SerializeField] private float m_ChanceOfThirdBarrelSpawn = 0;
     private float m_tileWidth;
     private float m_enemyWidth;
     public GameObject m_barrel;
+    private float m_barrelWidth;
+    private float m_barrelHeight;
     // Start is called before the first frame update
     void Start()
     {
+        int hasBarrel = -1;
         m_tileWidth = gameObject.GetComponentInChildren<Renderer>().bounds.size.x;
         m_enemyWidth = m_enemy.GetComponent<Renderer>().bounds.size.x;
+        m_barrelWidth = m_barrel.GetComponentInChildren<Renderer>().bounds.size.x;
+        m_barrelHeight = m_barrel.GetComponentInChildren<Renderer>().bounds.size.y;
         /// Gets the coordinate position of the tile the script is attached to.
         Vector3 centre = gameObject.transform.position;
+        if (Random.Range(0, 100) < m_ChanceOfBarrelSpawn)
+        {
+            int quadrant = Random.Range(0, 3);
+            if (quadrant == 0)
+            {
+                hasBarrel = 0;
+                Vector3 tempPos = new Vector3((centre.x + m_tileWidth / 2) - (m_tileWidth/13), centre.y + m_barrelHeight / 2, (centre.z + m_tileWidth / 2) - (m_tileWidth / 13));
+                Instantiate(m_barrel, tempPos, Quaternion.identity);
+                if (Random.Range(0, 100) < m_ChanceOfSecondBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.x -= m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+                if (Random.Range(0, 100) < m_ChanceOfThirdBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.z -= m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+            }
+            if (quadrant == 1)
+            {
+                hasBarrel = 1;
+                Vector3 tempPos = new Vector3((centre.x + m_tileWidth / 2) - (m_tileWidth / 13), centre.y + m_barrelHeight / 2, (centre.z - m_tileWidth / 2) + (m_tileWidth / 13));
+                Instantiate(m_barrel, tempPos, Quaternion.identity);
+                if (Random.Range(0, 100) < m_ChanceOfSecondBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.x -= m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+                if (Random.Range(0, 100) < m_ChanceOfThirdBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.z += m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+            }
+            if (quadrant == 2)
+            {
+                hasBarrel = 2;
+                Vector3 tempPos = new Vector3((centre.x - m_tileWidth / 2) + (m_tileWidth / 13), centre.y + m_barrelHeight / 2, (centre.z - m_tileWidth / 2) + (m_tileWidth / 13));
+                Instantiate(m_barrel, tempPos, Quaternion.identity);
+                if (Random.Range(0, 100) < m_ChanceOfSecondBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.x += m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+                if (Random.Range(0, 100) < m_ChanceOfThirdBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.z += m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+            }
+            if (quadrant == 3)
+            {
+                hasBarrel = 3;
+                Vector3 tempPos = new Vector3((centre.x - m_tileWidth / 2) + (m_tileWidth / 13), centre.y + m_barrelHeight / 2, (centre.z + m_tileWidth / 2) - (m_tileWidth / 13));
+                Instantiate(m_barrel, tempPos, Quaternion.identity);
+                if (Random.Range(0, 100) < m_ChanceOfSecondBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.x += m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+                if (Random.Range(0, 100) < m_ChanceOfThirdBarrelSpawn)
+                {
+                    Vector3 newTempPos = tempPos;
+                    newTempPos.z -= m_barrelWidth;
+                    Instantiate(m_barrel, newTempPos, Quaternion.identity);
+                }
+            }
+
+        }
         /// Calculates (based on probability) if an enemy will spawn.
-        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn)
+        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn && hasBarrel != 0)
         {
             /// Generates a random coordinate in first quadrant of the tile.
             Vector3 tempPos = new Vector3(centre.x + Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth/2) - (m_enemyWidth / 2) - (m_tileWidth / 20)), centre.y, centre.z + Random.Range((m_enemyWidth / 2) + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)));
@@ -32,7 +120,7 @@ public class SpawnObjectInRoom : MonoBehaviour
             Instantiate(m_enemy, tempPos, Quaternion.identity);
         }
         /// Calculates (based on probability) if an enemy will spawn.
-        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn)
+        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn && hasBarrel != 2)
         {
             /// Generates a random coordinate in first quadrant of the tile.
             Vector3 tempPos = new Vector3(centre.x - Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)), centre.y, centre.z - Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)));
@@ -40,7 +128,7 @@ public class SpawnObjectInRoom : MonoBehaviour
             Instantiate(m_enemy, tempPos, Quaternion.identity);
         }
         /// Calculates (based on probability) if an enemy will spawn.
-        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn)
+        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn && hasBarrel != 3)
         {
             /// Generates a random coordinate in first quadrant of the tile.
             Vector3 tempPos = new Vector3(centre.x - Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)), centre.y, centre.z + Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)));
@@ -48,7 +136,7 @@ public class SpawnObjectInRoom : MonoBehaviour
             Instantiate(m_enemy, tempPos, Quaternion.identity);
         }
         /// Calculates (based on probability) if an enemy will spawn.
-        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn)
+        if (Random.Range(0, 100) < m_ChanceOfEnemySpawn && hasBarrel != 1)
         {
             /// Generates a random coordinate in first quadrant of the tile.
             Vector3 tempPos = new Vector3(centre.x + Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) -( m_enemyWidth / 2) - (m_tileWidth / 20)), centre.y, centre.z - Random.Range(m_enemyWidth / 2 + (m_tileWidth / 16), (m_tileWidth / 2) - (m_enemyWidth / 2) - (m_tileWidth / 20)));
