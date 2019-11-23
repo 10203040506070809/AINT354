1e7fcbb12afc56a9f587fa6a2a4fa037820f2b62
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2019-11-23 19:58:26 +0000

    Commented ItemBreak script

--- Dungeon Crawler/Assets/Test Scripts/ItemBreak.cs
+++ Dungeon Crawler/Assets/Test Scripts/ItemBreak.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ItemBreak : MonoBehaviour
 {
+    /// <summary>
+    /// Objects take 2 hits to break.
+    /// </summary>
     int m_health = 2;
+    /// <summary>
+    /// Reduces the item's health by 1 for every hit and possibly spawns an item.
+    /// </summary>
     public void TakeDamage()
     {
+        /// Reduces health by 1.
         m_health -= 1;
+        /// Checks if the health is less than 0.
         if (m_health <= 0)
         {
+            /// Calls the function to drop an item.
             gameObject.GetComponent<DropItem>().Drop(gameObject.transform.position, gameObject);
+            /// Destroys the item.
             Destroy(gameObject);
         }
     }
